5573b779f86a0b5a991d0160e58221fe3f33d692
정태환 <jeongtaehwan@jeongtaehwan-ui-MacBookPro.local>
2021-11-23 23:07:40 +0900

    Update : files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      CRA로 만든 프로젝트에 Storybook을 사용해보았습니다 🤾🏻‍♂️
     </div>
   );
 }
